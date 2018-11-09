1d40ab0349390667a67ff7cf302118983fc0df3f
Mohammad-Mahdi1999 <44753085+Mohammad-Mahdi1999@users.noreply.github.com>
2018-11-10 00:40:13 +0330

    start

---
+++ Numbers/Numbers.cpp
@@ -0,0 +1,20 @@
+// In the name of God !
+//Numbers
+//v1.0
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	int i,j;
+	for (i = 5; i >= 1; i--)
+	{
+		for (j = 1; j <= i; j++)
+			cout << j << " ";
+		cout << endl;
+	}
+	cin.get();
+	cin.ignore();
+}
+//Created by /%++;; Mohammad-Mahdi Farah-bakhsh ;;++%\
